aa7d30b86ca188e4ef1cb3fe5129c3095baff53c
minhalo113 <minha@MSI>
2025-03-24 03:30:43 -0600

    update dashboard frontend

--- dashboard/src/navigation/allNav.js
+++ dashboard/src/navigation/allNav.js
@@ -21,11 +21,25 @@ export const allNav = [
         path: '/admin/dashboard'
     },
     {
-        id : 2,
-        title : 'Orders',
-        icon : <AiOutlineShoppingCart />,
+        id : 6,
+        title : 'Add Product',
+        icon : <IoMdAdd />,
         role : 'admin',
-        path: '/admin/dashboard/orders'
+        path: '/admin/dashboard/add-product'
+    },     
+    {
+        id : 7,
+        title : 'All Product',
+        icon : <MdViewList />,
+        role : 'admin',
+        path: '/admin/dashboard/products'
+    },
+    {
+        id : 8,
+        title : 'Discount Product',
+        icon : <TbBasketDiscount />,
+        role : 'admin',
+        path: '/admin/dashboard/discount-product'
     },
     {
         id : 3,
@@ -35,104 +49,25 @@ export const allNav = [
         path: '/admin/dashboard/category'
     },
     {
-        id : 4,
-        title : 'Sellers',
-        icon : <FaUsers   />,
+        id : 2,
+        title : 'Orders',
+        icon : <AiOutlineShoppingCart />,
         role : 'admin',
-        path: '/admin/dashboard/sellers'
+        path: '/admin/dashboard/orders'
     },
     {
-        id : 5,
+        id : 4,
         title : 'Payment Request',
         icon : <MdPayment />,
         role : 'admin',
         path: '/admin/dashboard/payment-request'
     },
     {
-        id : 6,
-        title : 'Deactive Sellers',
-        icon : <FaUserTimes />,
-        role : 'admin',
-        path: '/admin/dashboard/deactive-sellers'
-    },
-    {
-        id : 7,
-        title : 'Seller Request',
-        icon : <FaCodePullRequest />,
-        role : 'admin',
-        path: '/admin/dashboard/sellers-request'
-    },
-    {
-        id : 8,
+        id : 5,
         title : 'Live Chat',
         icon : <IoIosChatbubbles />,
         role : 'admin',
         path: '/admin/dashboard/chat-sellers'
     },
-    {
-        id : 9,
-        title : 'Dashboard',
-        icon : <AiOutlineDashboard />,
-        role : 'seller',
-        path: '/seller/dashboard'
-    },
-    {
-        id : 10,
-        title : 'Add Product',
-        icon : <IoMdAdd />,
-        role : 'seller',
-        path: '/seller/dashboard/add-product'
-    },     
-    {
-        id : 11,
-        title : 'All Product',
-        icon : <MdViewList />,
-        role : 'seller',
-        path: '/seller/dashboard/products'
-    },
-    {
-        id : 12,
-        title : 'Discount Product',
-        icon : <TbBasketDiscount />,
-        role : 'seller',
-        path: '/seller/dashboard/discount-product'
-    },
-    {
-        id : 13,
-        title : 'Orders',
-        icon : <BsCartCheck />,
-        role : 'seller',
-        path: '/seller/dashboard/orders'
-    },
-    {
-        id : 14,
-        title : 'Payments',
-        icon : <MdPayment />,
-        role : 'seller',
-        path: '/seller/dashboard/payments'
-    },
-    {
-        id : 15,
-        title : 'Chat-Customer',
-        icon : <IoChatbubbles />,
-        role : 'seller',
-        path: '/seller/dashboard/chat-customer'
-    },
-    {
-        id : 16,
-        title : 'Chat-Support',
-        icon : <BsFillChatQuoteFill />,
-        role : 'seller',
-        path: '/seller/dashboard/chat-support'
-    },
-    {
-        id : 17,
-        title : 'Profile',
-        icon : <CgProfile />,
-        role : 'seller',
-        path: '/seller/dashboard/profile'
-    }
-
-
 
 ]
--- dashboard/src/router/routes/privateRoutesAdmin.jsx
+++ dashboard/src/router/routes/privateRoutesAdmin.jsx
@@ -1,10 +1,52 @@
 import { lazy } from "react"
 import AdminDashboard from './../../views/admin/AdminDashboard';
+import Orders from "../../views/admin/Order";
+import Category from "../../views/admin/Category";
+import ChatSeller from "../../views/admin/ChatSeller";
+import PaymentRequest from "../../views/admin/PaymentRequest";
+import AddProduct from './../../views/admin/AddProduct';
+import Products from './../../views/admin/Products';
+import DiscountProducts from './../../views/admin/DiscountProducts';
 
 export const privateRoutesAdmin = [
     {
         path: "/admin/dashboard",
         element: <AdminDashboard/>,
         role: 'admin'
-    }
+    },
+    {
+        path: 'admin/dashboard/orders',
+        element : <Orders/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/category',
+        element : <Category/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/chat-sellers',
+        element : <ChatSeller/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/payment-request',
+        element : <PaymentRequest/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/add-product',
+        element : <AddProduct/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/products',
+        element : <Products/> ,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/discount-product',
+        element : <DiscountProducts/> ,
+        role : 'admin'
+    },
 ]
