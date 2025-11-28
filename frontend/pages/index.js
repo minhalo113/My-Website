bf91ade7d5448532a8610a1a865a97c27960d20c
minhalo113 <minha@MSI>
2025-11-28 14:17:09 -0700

    home lag

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,27 @@
-import Banner from './home/Banner'
-import HomeCategory from './home/HomeCategory'
-import CategoryShowCase from './home/CategoryShowCase'
-import LocationSprade from './home/LocationSprade'
-import AboutUs from './home/AboutUs'
-import AppSection from './home/AppSection'
-import Sponsor from './home/Sponsor'
-import SEO from '../components/SEO'
-import api from '../src/api/api'
-
+import React from 'react';
+import dynamic from 'next/dynamic';
+import Banner from './home/Banner';
+import SEO from '../components/SEO';
+import api from '../src/api/api';
 import PropTypes from 'prop-types';
+
+const HomeCategory = dynamic(() => import('./home/HomeCategory'), {
+  loading: () => <div className="min-h-[200px] animate-pulse bg-gray-50" />,
+  ssr: true
+});
+
+const CategoryShowCase = dynamic(() => import('./home/CategoryShowCase'), {
+  loading: () => <div className="min-h-[500px] animate-pulse bg-gray-50" />,
+  ssr: true
+});
+
+const AboutUs = dynamic(() => import('./home/AboutUs'), {
+  ssr: false
+});
+
+// const LocationSprade = dynamic(() => import('./home/LocationSprade'));
+// const AppSection = dynamic(() => import('./home/AppSection'));
+// const Sponsor = dynamic(() => import('./home/Sponsor'));
 
 export const Home = ({ products, categorys, swiperItems, featuredCategories, showcaseProducts }) => {
   return (
@@ -20,20 +33,16 @@ export const Home = ({ products, categorys, swiperItems, featuredCategories, sho
       />
       <div>
         <Banner products={products} categorys={categorys} swiperItems={swiperItems} />
+
         <HomeCategory categories={featuredCategories} />
         <CategoryShowCase initialProducts={showcaseProducts} allCategories={categorys} />
-
-        {/* <Register/> */}
-        {/* <LocationSprade/> */}
         <AboutUs />
-        {/*<AppSection/>*/}
-        {/* <Sponsor/> */}
       </div>
     </>
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   try {
     const [productsRes, categorysRes, swiperRes, featuredRes, showcaseRes] = await Promise.all([
       api.get('/customers-products-get'),
@@ -56,6 +65,7 @@ export async function getServerSideProps() {
         featuredCategories: featuredRes.data.categories || [],
         showcaseProducts: showcaseRes.data.products || [],
       },
+      revalidate: 60,
     };
   } catch (error) {
     console.error("Error fetching data for Home page:", error);
@@ -67,6 +77,7 @@ export async function getServerSideProps() {
         featuredCategories: [],
         showcaseProducts: [],
       },
+      revalidate: 60,
     };
   }
 }
@@ -79,4 +90,4 @@ Home.propTypes = {
   showcaseProducts: PropTypes.array,
 };
 
-export default Home
+export default Home;
