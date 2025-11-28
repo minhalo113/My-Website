31f30cf6d2fa005560b5c6fb3852942194fdd92f
minhalo113 <minha@MSI>
2025-11-28 08:57:47 -0700

    update config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,29 @@
 const nextConfig = {
   trailingSlash: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.adidas.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
