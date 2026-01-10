346e009502cb4c34e3de3d0c1fbefcff8af9f734
minhalo113 <minha@MSI>
2026-01-09 17:49:57 -0700

    update

--- backend/controllers/home/homeController.js
+++ backend/controllers/home/homeController.js
@@ -236,8 +236,6 @@ class homeControllers {
         const { productId } = req.params;
         try {
             let product = await productModel.findOne({ _id: productId, isHidden: false }).lean();
-            console.log('test')
-            console.log(product);
             if (!product) {
                 return responseReturn(res, 404, { error: 'Product not found' });
             }
