618d563a682dc9ef34ff678c8053e47bb10eecdf
minhalo113 <minha@MSI>
2026-02-04 19:21:34 -0700

    update

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -67,7 +67,7 @@ function MyApp({ Component, pageProps }) {
           <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <!-- TikTok Pixel Code Start -->
+          {/* TikTok Pixel Code Start */}
           <script>
             !function (w, d, t) {
               w.TiktokAnalyticsObject = t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) }};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
@@ -79,7 +79,7 @@ function MyApp({ Component, pageProps }) {
             ttq.page();
           }(window, document, 'ttq');
           </script>
-          <!-- TikTok Pixel Code End -->
+          {/* TikTok Pixel Code End */}
         </Head>
         <CartProvider>
 
